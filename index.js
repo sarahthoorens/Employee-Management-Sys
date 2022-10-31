93adf404cead159ab86fc2c71081326274f7dbec
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-31 03:50:15 -0500

    debugged the add an employee function and completed update an employee's role function.

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function addEmployee () {
     dbConnect.query(rolesTable, (err, response) => {
         if (err) throw err;
         response.forEach((role) => {
-            roles.push(`${role.role_id} - ${role.title}`);
+            roles.push(`${role.title}`);
     });
     let managers = [];
     let managerTable = `SELECT first_name,last_name, title, role_id 
@@ -201,32 +201,77 @@ function addEmployee () {
     },
   ])
   .then ((answers) => {
-    let role_id = answers.employeeRole.split(' ')[0];
-    let title = answers.employeeRole.split(' ')[2];
-    let managerName = answers.employeeManager
+    const managerName = answers.employeeManager
     console.log(managerName);
+    let role_id
+    let title = answers.employeeRole;
+    response.forEach((role) => {
+        if (title === role.title) {
+        role_id = role.role_id;
+        console.log(`this is the title ${title}`);
+    }});
     let answersArray = [answers.employeeFirstName, answers.employeeLastName, role_id, title, managerName];
     console.log(`Answers array ${answersArray}`);
-    let table = `INSERT INTO employees (first_name, last_name, role_id, title, name_of_manager) VALUES (?, ?, ?, ?, ?);`;
-    dbConnect.query(table, answersArray, ((err) => {
+    const newEmptable = `INSERT INTO employees (first_name, last_name, role_id, title, name_of_manager) VALUES (?, ?, ?, ?, ?);`;
+    dbConnect.query(newEmptable, answersArray, ((err) => {
         if (err) throw err;
         console.log('New employee added successfully.');
         getAllEmployees();
     }))
  });
-})});
-};
- function updateEmployeeRole () {
-    const question = inquirer 
+})})}
+;
+ const updateEmployeeRole = () => {
+    let employeeList = []
+    let employeesTable = `SELECT first_name, last_name, id FROM employees ORDER BY title ASC;`;
+    dbConnect.query(employeesTable, (err, response) => {
+        if (err) throw err;
+        response.forEach((employees) => {
+            employeeList.push(`${employees.first_name} ${employees.last_name} ${employees.id}`);
+    });
+    let roles = []
+    let rolesTable = `SELECT * FROM role ORDER BY title ASC;`;
+    dbConnect.query(rolesTable, (err, response) => {
+        if (err) throw err;
+        response.forEach((role) => {
+            roles.push(`${role.role_id} - ${role.title}`);
+    });
+    inquirer 
         .prompt ([
         {
-            type: 'list', 
-            name: 'employeeRoleUpdate',
-            message: `Which role do you want to assign the employee?`,
-            // choices: [refer to roles table]
+            type: 'list',
+            name: 'employeeToUpdate',
+            message: 'Which employee would you like to update?',
+            choices: employeeList
         },
-])
-};
+        {
+            type: 'list', 
+            name: 'employeeNewRole',
+            message: `Which role do you want to assign the employee?`,
+            choices: roles
+        },
+]) .then((answers) => {
+    const employeeIdToUpdate = answers.employeeToUpdate.split('')[2];
+    const newRoleId = answers.employeeNewRole.split(' ')[0];
+    let newRole;
+    response.forEach((role) => {
+        if (newRoleId === role.role_id) {
+        newRole = role.title;
+        console.log(`this is the new role ${newrole}`);
+    }});
+
+    const updateEmpArray = [newRoleId, newRole, employeeIdToUpdate]
+    let selectedEmployee = `UPDATE employees 
+                            SET employees.role_id = ? and SET employees.title = ? WHERE 
+                            employees.id = ?`
+       dbConnect.query(selectedEmployee, updateEmpArray, (err) => {
+      if (err) throw err;
+      console.log('Employee has been updated successfully.');
+        getAllEmployees();
+        })
+    })
+})})
+}
 promptUser();
 
 
